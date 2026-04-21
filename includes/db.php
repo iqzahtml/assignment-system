4af0a37a798caed80305a431f482cdcf585b31c0
iqzahtml <iqzahfarhah@gmail.com>
2026-04-22 00:11:00 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,14 @@
 <?php
-// database connection
+// connect database
 $conn = new mysqli("localhost", "root", "", "assignment_system");
 
+// check connection
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
+}
+
+// start session
+if(session_status() === PHP_SESSION_NONE){
+    session_start();
 }
 ?>
