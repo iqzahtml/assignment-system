02037f9b5d250ab0355381dfd997368648360ddb
nabilkudin44 <nabilkudin44@gmail.com>
2026-04-21 22:25:01 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -42,7 +42,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="dashboard.php">Submission Assignment</a>
+        <a class="navbar-brand" href="dashboard.php">   📝 Submission Assignment</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
