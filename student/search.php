147a386595aaacf2fb0b54b15ae4288ec36c6164
iqzahtml <iqzahfarhah@gmail.com>
2026-04-21 23:48:22 +0800

    Create search.php

---
+++ student/search.php
@@ -0,0 +1,17 @@
+<?php include "../includes/db.php"; ?>
+
+$key = $_GET['key'];
+
+// search assignment
+$sql = "SELECT * FROM assignments WHERE title LIKE ?";
+$stmt = $conn->prepare($sql);
+
+$search = "%$key%";
+$stmt->bind_param("s", $search);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+while($row = $result->fetch_assoc()){
+    echo $row['title'] . "<br>";
+}
